af5857b6b2784f3b0b4f771e3dff819d9bb2d4ee
Abdulla Vennakode <abdullacp9048@gmail.com>
2026-02-16 21:40:27 +0530

    created ClinicServices

--- Backend/src/Composer/composer.js
+++ Backend/src/Composer/composer.js
@@ -3,6 +3,8 @@ import TokenGenerator from "../Utils/TokenGenerator.js";
 import AuthServices from "../Service/AuthServices.js";
 import AuthContoller from "../Controller/AuthController.js";
 import ClinicRepository from "../Repository/ClinicRepository.js";
+import ClinicService from "../Service/ClinicService.js";
+
 
 
 
@@ -11,3 +13,4 @@ export const TokenGen = new TokenGenerator();
 export const AuthSer = new AuthServices();
 export const AuthCont = new AuthContoller();
 export const ClinicRepo = new ClinicRepository();
+export const ClinicSer = new ClinicService();
---
+++ Backend/src/Service/ClinicService.js
@@ -0,0 +1,55 @@
+import { ClinicRepo } from "../Composer/composer.js";
+
+
+class ClinicService {
+    async clinicCreate(name, place, ownerId, start, end, averageConsultationTime, status) {
+        try {
+            if(!name || !place || !ownerId || !start || !end || !status) throw new Error("name, place, ownerId, time and status are required");
+            return await ClinicRepo.create(name, place, ownerId, start, end, averageConsultationTime, status);
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async allClinics() {
+        try {
+            const allClinicsList = await ClinicRepo.findAll();
+            if(allClinicsList.length < 1) throw new Error("No Clinics are Listed");
+
+            return allClinicsList;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async findClinicById (id) {
+        try {
+            if(!id) throw new Error("Clinic ID is required");
+            const clinic = await ClinicRepo.findById(id);
+
+            if(!clinic) throw new Error("No Clinic Found");
+            return clinic;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async findClinicByOwner(ownerId) {
+        try {
+            if(!ownerId) throw new Error("OwnerId is required");
+            const clinics = await ClinicRepo.findByOwner(ownerId);
+
+            if(!clinics) throw new Error("No Clinic Listed By the provided User");
+            
+            return clinics;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default ClinicService;
